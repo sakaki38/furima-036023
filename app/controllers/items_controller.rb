c5f84eb684c87c0e2550563c5b66e66827baeabd
sakaki38 <h.sakaki38@gmail.com>
2021-11-12 12:35:30 +0900

    修正２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,5 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
-  before_action :move_to_index, except: [:index, :new]
 
   #def index
     #@items = Item.all
@@ -26,7 +25,4 @@ class ItemsController < ApplicationController
                                  :price).merge(user_id: current_user.id)
   end
 
-  def move_to_index
-    redirect_to action: :index unless user_signed_in?
-  end
 end
